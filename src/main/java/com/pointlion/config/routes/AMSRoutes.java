3be66d177dd9a5142a9cb041a0729f307e62ec68
king <king@163.com>
2022-11-16 16:08:39 +0800

    欣迪firstcommit

--- src/main/java/com/pointlion/config/routes/AMSRoutes.java
+++ src/main/java/com/pointlion/config/routes/AMSRoutes.java
@@ -16,13 +16,13 @@ public class AMSRoutes extends Routes{
 	public void config() {
 		setBaseViewPath("/WEB-INF/admin/ams");
 		add("/admin/ams/asset",AmsAssetController.class,"/asset");//资产管理----资产字典
-		add("/admin/ams/repair",AmsAssetRepairController.class,"/repair");//资产报修----报修
-		add("/admin/ams/borrow",AmsAssetBorrowController.class,"/borrow");//资产借用----借用其他部门的资产设备
-		add("/admin/ams/dispose",AmsAssetDisposeController.class,"/dispose");//资产处置----资产处理
-		add("/admin/ams/receive",AmsAssetReceiveController.class,"/receive");//资产领用----入库之后，从数据库中领用到本部门
-		add("/admin/ams/allot",AmsAssetAllotController.class,"/allot");//资产调配---把自己部门的资产调配给其他部门
-		add("/admin/ams/need",AmsAssetNeedController.class,"/need");//资产需要----需要购买
-		add("/admin/ams/signin",AmsAssetSignInController.class,"/signin");//资产录入----录入新的资产
+		//add("/admin/ams/repair",AmsAssetRepairController.class,"/repair");//资产报修----报修
+		//add("/admin/ams/borrow",AmsAssetBorrowController.class,"/borrow");//资产借用----借用其他部门的资产设备
+		//add("/admin/ams/dispose",AmsAssetDisposeController.class,"/dispose");//资产处置----资产处理
+		//add("/admin/ams/receive",AmsAssetReceiveController.class,"/receive");//资产领用----入库之后，从数据库中领用到本部门
+		//add("/admin/ams/allot",AmsAssetAllotController.class,"/allot");//资产调配---把自己部门的资产调配给其他部门
+		//add("/admin/ams/need",AmsAssetNeedController.class,"/need");//资产需要----需要购买
+		//add("/admin/ams/signin",AmsAssetSignInController.class,"/signin");//资产录入----录入新的资产
 	}
 
 }
--- src/main/java/com/pointlion/config/routes/OARoutes.java
+++ src/main/java/com/pointlion/config/routes/OARoutes.java
@@ -85,22 +85,22 @@ public class OARoutes extends Routes{
 		//add("/admin/oa/workplan",WorkPlanController.class,"/workplan");//工作计划
 		add("/admin/oa/workplan",OaWorkPlanController.class,"/workplan");//工作计划
 		//公文
-		add("/admin/oa/bumph",BumphController.class,"/bumph");//公文管理---内部发文，收文转发
+		//add("/admin/oa/bumph",BumphController.class,"/bumph");//公文管理---内部发文，收文转发
 		
 		add("/admin/oa/outm",OaOutmController.class,"/outm");//公文管理---内部发文，收文转发
 		
 		add("/admin/oa/notices",OaNoticesController.class,"/notices");
-		add("/admin/oa/documents",OaDocumentsController.class,"/documents");
+		//add("/admin/oa/documents",OaDocumentsController.class,"/documents");
 		
 		add("/admin/oa/picnews",OaPicNewsController.class,"/picnews");
-		add("/admin/oa/email",OaEmailController.class,"/email");
-		add("/admin/oa/showinfo",OaShowinfoController.class,"/showinfo");
+		//add("/admin/oa/email",OaEmailController.class,"/email");
+		//add("/admin/oa/showinfo",OaShowinfoController.class,"/showinfo");
 		add("/admin/oa/meetroom",OaMeetroomController.class,"/meetroom");
 		add("/admin/oa/dict",OaDictController.class,"/dict");
 		add("/admin/oa/test",OaTestController.class,"/test");
-		add("/admin/oa/hotline",OaHotlineController.class,"/hotline");
-		add("/admin/oa/senddoc",OaSenddocController.class,"/senddoc");
-		add("/admin/oa/letter",OaLetterController.class,"/letter");
+		//add("/admin/oa/hotline",OaHotlineController.class,"/hotline");
+		//add("/admin/oa/senddoc",OaSenddocController.class,"/senddoc");
+		//add("/admin/oa/letter",OaLetterController.class,"/letter");
 		add("/admin/oa/stephistory",OaStepHistoryController.class,"/stephistory");
 		add("/admin/oa/edletter",OaEdLetterController.class,"/edletter");
 		add("/admin/oa/departnotices",OaDepartNoticesController.class,"/departnotices");
@@ -118,39 +118,39 @@ public class OARoutes extends Routes{
 		add("/admin/oa/project",OaProjectController.class,"/project");//项目管理
 		//合同管理
 		add("/admin/oa/contract",OaContractController.class,"/contract");//合同管理
-		add("/admin/oa/contract/apply",OaContractApplyController.class,"/contract/apply");//合同申请
-		add("/admin/oa/contract/change",OaContractChangeController.class,"/contract/change");//合同变更
-		add("/admin/oa/contract/invoice",OaContractInvoiceController.class,"/contract/invoice");//合同开票
-		add("/admin/oa/contract/pay",OaContractPayController.class,"/contract/pay");//合同付款
-		add("/admin/oa/contract/stop",OaContractStopController.class,"/contract/stop");//合同终止
+		//add("/admin/oa/contract/apply",OaContractApplyController.class,"/contract/apply");//合同申请
+		//add("/admin/oa/contract/change",OaContractChangeController.class,"/contract/change");//合同变更
+		//add("/admin/oa/contract/invoice",OaContractInvoiceController.class,"/contract/invoice");//合同开票
+		//add("/admin/oa/contract/pay",OaContractPayController.class,"/contract/pay");//合同付款
+		//add("/admin/oa/contract/stop",OaContractStopController.class,"/contract/stop");//合同终止
 		//在线办公---各种各样的申请申请
 		add("/admin/oa/workflow/flowtask",FlowTaskController.class,"/workflow/flowtask");//处理通用任务
-		add("/admin/oa/apply/common", OaApplyCommonController.class,"/apply/common");//通用申请
+		//add("/admin/oa/apply/common", OaApplyCommonController.class,"/apply/common");//通用申请
 		add("/admin/oa/apply/businesstravel", OaApplyBusinessTravelController.class,"/apply/businesstravel");//出差申请
 		add("/admin/oa/apply/businesstravelreport", OaApplyBusinessTravelReportController.class,"/apply/businesstravelreport");//出差报告
-		add("/admin/oa/apply/businesscard",OaApplyBusinessCardController.class,"/apply/businesscard");//名片印刷申请
+		//add("/admin/oa/apply/businesscard",OaApplyBusinessCardController.class,"/apply/businesscard");//名片印刷申请
 		add("/admin/oa/apply/buy",OaApplyBuyController.class,"/apply/buy");//采购申请
 		add("/admin/oa/apply/custom",OaApplyCustomController.class,"/apply/custom");//自定义流程
 		add("/admin/oa/apply/leave",OaApplyLeaveController.class,"/apply/leave");//请销假
-		add("/admin/oa/apply/usecar",OaApplyUseCarController.class,"/apply/usecar");//用车申请
-		add("/admin/oa/apply/hotel",OaApplyHotelController.class,"/apply/hotel");//宾馆预定申请
-		add("/admin/oa/apply/officeobjectbuy", OaApplyOfficeObjectBuyController.class,"/apply/officeobjectbuy");//办公用品申购
-		add("/admin/oa/apply/officeobject",OaApplyOfficeObjectController.class,"/apply/officeobject");//办公用品领用
-		add("/admin/oa/apply/seal",OaApplySealController.class,"/apply/seal");//用章申请
-		add("/admin/oa/apply/trainticket",OaApplyTrainTicketController.class,"/apply/trainticket");//车船票申请
-		add("/admin/oa/apply/usercarwork",OaApplyUsercarWorkController.class,"/apply/usercarwork");//私车公用补助申请
-		add("/admin/oa/apply/gift",OaApplyGiftController.class,"/apply/gift");//礼品申请
-		add("/admin/oa/apply/meetroom",OaApplyMeetroomController.class,"/apply/meetroom");//会议室申请
+		//add("/admin/oa/apply/usecar",OaApplyUseCarController.class,"/apply/usecar");//用车申请
+		//add("/admin/oa/apply/hotel",OaApplyHotelController.class,"/apply/hotel");//宾馆预定申请
+		//add("/admin/oa/apply/officeobjectbuy", OaApplyOfficeObjectBuyController.class,"/apply/officeobjectbuy");//办公用品申购
+		//add("/admin/oa/apply/officeobject",OaApplyOfficeObjectController.class,"/apply/officeobject");//办公用品领用
+		//add("/admin/oa/apply/seal",OaApplySealController.class,"/apply/seal");//用章申请
+		//add("/admin/oa/apply/trainticket",OaApplyTrainTicketController.class,"/apply/trainticket");//车船票申请
+		//add("/admin/oa/apply/usercarwork",OaApplyUsercarWorkController.class,"/apply/usercarwork");//私车公用补助申请
+		//add("/admin/oa/apply/gift",OaApplyGiftController.class,"/apply/gift");//礼品申请
+		//add("/admin/oa/apply/meetroom",OaApplyMeetroomController.class,"/apply/meetroom");//会议室申请
 		add("/admin/oa/apply/userchangestation",OaApplyUserChangeStationController.class,"/apply/userchangestation");//调岗申请
 		add("/admin/oa/apply/userdimission",OaApplyUserDimissionController.class,"/apply/userdimission");//离职申请
 		add("/admin/oa/apply/userregular",OaApplyUserRegularController.class,"/apply/userregular");//转正申请
 		add("/admin/oa/apply/cost",OaApplyCostController.class,"/apply/cost");//转正申请
-		add("/admin/oa/apply/finance",OaApplyFinanceController.class,"/apply/finance");//财务类申请
+		//add("/admin/oa/apply/finance",OaApplyFinanceController.class,"/apply/finance");//财务类申请
 		add("/admin/oa/apply/workovertime", OaApplyWorkOvertimeController.class,"/apply/workovertime");//加班申请
-		add("/admin/oa/apply/bankaccount",OaApplyBankAccountController.class,"/apply/bankaccount");//银行卡开销户申请
-		add("/admin/oa/apply/borrowmoney", OaApplyBorrowMoneyController.class,"/apply/borrowmoney");//借款申请
+		//add("/admin/oa/apply/bankaccount",OaApplyBankAccountController.class,"/apply/bankaccount");//银行卡开销户申请
+		//add("/admin/oa/apply/borrowmoney", OaApplyBorrowMoneyController.class,"/apply/borrowmoney");//借款申请
 		//主数据管理
-		add("/admin/oa/dct/resourcecar",DctResourceCarController.class,"/dct/resourcecar");//车辆信息管理
+		add("/admin/oa/dct/reso/admin/oa/apply/usecar/urcecar",DctResourceCarController.class,"/dct/resourcecar");//车辆信息管理
 		add("/admin/oa/dct/resourcemeetroom",DctResourceMeetroomController.class,"/dct/resourcemeetroom");//会议室管理
 		//流程在线编辑器和流程跟踪所用路由
 		add("/admin/oa/process-instance/highlights",ProcessInstanceHighlightsResource.class);//modeler
