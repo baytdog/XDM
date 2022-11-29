d050d1d5e4ac9238b14dfc9bc03b4b871ae7ac56
king <king@163.com>
2022-11-29 18:23:41 +0800

    1

--- src/main/java/com/pointlion/mvc/admin/xdm/xdworkexper/XdWorkExperService.java
+++ src/main/java/com/pointlion/mvc/admin/xdm/xdworkexper/XdWorkExperService.java
@@ -58,6 +58,9 @@ public class XdWorkExperService{
     	}
 	}
 
+
+
+
     public List<XdWorkExper> getWorkExperList(String employeeId) {
 		String sql="select * from "+TABLE_NAME;
 //		String sql="select * from "+TABLE_NAME+"where eid='"+employeeId+"'";
